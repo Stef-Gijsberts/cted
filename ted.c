77b64f0a7b08e3f4725e6d69a838b1fcf4826663
Stef Gijsberts <stef@stef.link>
2025-12-26 13:03:20 +0100

    Improve cursor

--- ted.c
+++ ted.c
@@ -1,84 +1,101 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
+
+enum status { STATUS_INITIAL, STATUS_QUITTING };
 
 struct state {
-    char buf[1024];
-    FILE *fp;
-    size_t cursor;
+	size_t cursor;
+	FILE *fp;
+	enum status status;
+	char buf[1024];
 };
 
 void init(struct state *s) {
-    const char *path = "./text.txt";
-    s->fp = fopen(path, "r");
+	const char *path = "./text.txt";
+	s->fp = fopen(path, "r");
 
-    size_t n = fread(s->buf, 1, sizeof(s->buf) - 1, s->fp);
-    s->buf[n] = '\0';
+	size_t n = fread(s->buf, 1, sizeof(s->buf) - 1, s->fp);
+	s->buf[n] = '\0';
 }
 
-void cleanup(struct state *s) {
-    fclose(s->fp);
-}
+void cleanup(struct state *s) { fclose(s->fp); }
 
 /**
  * Move the cursor. The top left is (1, 1).
  */
-void move_cursor(int line, int column) {
-    printf("\033[%d;%dH", line, column);
-}
+void move_cursor(int row, int column) { printf("\033[%d;%dH", row, column); }
 
-void clear_screen() {
-    printf("\033[2J");
-}
+void clear_screen() { printf("\033[2J"); }
 
-void clear_scrollback_buffer() {
-    printf("\033[3J");
+void clear_scrollback_buffer() { printf("\033[3J"); }
+
+struct dimensions {
+	int cols;
+	int rows;
+};
+
+struct dimensions get_dimensions() {
+	struct winsize w;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) {
+		w.ws_col = 100;
+		w.ws_row = 24;
+	}
+
+	struct dimensions result = {.cols = w.ws_col, .rows = w.ws_row};
+
+	return result;
 }
 
 void present(struct state *s) {
-    struct winsize w;
+	struct dimensions d = get_dimensions();
 
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) {
-        w.ws_col = 100;
-        w.ws_row = 24;
-    }
+	clear_screen();
+	clear_scrollback_buffer();
+	move_cursor(1, 1);
+	puts(s->buf);
 
-    clear_screen();
-    clear_scrollback_buffer();
-    move_cursor(1, 1);
-    puts(s->buf);
+	move_cursor(d.rows, d.cols - 10);
+	printf("== %d ==", s->cursor);
 
-    move_cursor(w.ws_row, w.ws_col - 10);
-    printf("== %d ==", s->cursor);
+	move_cursor(1, s->cursor);
 
-    fflush(stdout);
+	fflush(stdout);
 }
 
 int main() {
-    system("stty -icanon -echo");
+	system("stty -icanon -echo");
 
-    struct state s = {
-        .buf = { 0 },
-        .fp = NULL,
-        .cursor = 0,
-    };
+	struct state s = {
+	    .buf = {0},
+	    .fp = NULL,
+	    .cursor = 0,
+	    .status = STATUS_INITIAL,
+	};
 
-    init(&s);
+	init(&s);
 
-    for (;;) {
-        int c = getchar();
-        
-        if (c == 'h') {
-            s.cursor--;
-        }
-        if (c == 'l') {
-            s.cursor++;
-        }
+	while (s.status != STATUS_QUITTING) {
+		int c = getchar();
 
-        present(&s);
-    }
+		if (c == 'h') {
+			s.cursor--;
+		}
+		if (c == 'l') {
+			s.cursor++;
+		}
+		if (c == 'q') {
+			s.status = STATUS_QUITTING;
+		}
 
-    cleanup(&s);
-    return 0;
+		present(&s);
+	}
+
+	cleanup(&s);
+	system("stty icanon echo");
+
+	return 0;
 }
